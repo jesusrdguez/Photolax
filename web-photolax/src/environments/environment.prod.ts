2cdfca9a7b19f798fb6dd970f00c5bb79556205a
jesusrdguez <jesusrodrimerc99@gmail.com>
2025-06-05 22:42:54 +0200

    Instalamos @types/node para Typescript

--- web-photolax/src/environments/environment.prod.ts
+++ web-photolax/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+declare const process: {
+  env: {
+    [key: string]: string;
+  }
+};
+
 export const environment = {
     production: true,
     apiUrl: process.env['API_URL'] || 'https://photolax.onrender.com/api-photolax'
